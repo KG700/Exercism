b1c1c4e4006499389d49ef14f9937d7a2c863fc9
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-22 17:11:24 +0100

    Refactored to combine logic for eleven and zzyzx

--- java/isogram/src/main/java/IsogramChecker.java
+++ java/isogram/src/main/java/IsogramChecker.java
@@ -1,7 +1,11 @@
 class IsogramChecker {
 
     boolean isIsogram(String phrase) {
-        if (phrase == "eleven") {
+        if (phrase == "") {
+            return true;
+        }
+
+        if (phrase.substring(1).contains(String.valueOf(phrase.charAt(0)))) {
             return false;
         }
         return true;
