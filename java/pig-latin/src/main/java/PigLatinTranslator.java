dcea31cc72906d176199b03a8a1f20b1465382a2
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-24 20:07:14 +0100

    Passed testWordBeginningWithP

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,5 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
 class PigLatinTranslator {
+
     String translate(String word) {
-        return word + "ay";
+        String pigWord = word;
+
+        String[] vowels = new String[] { "a", "e", "i", "o", "u" };
+        List<String> vowelsList = Arrays.asList(vowels);
+
+        String firstLetter = String.valueOf(word.charAt(0));
+
+        if( !vowelsList.contains(firstLetter) ) {
+            pigWord = word.substring(1) + firstLetter;
+        }
+
+        return pigWord + "ay";
     }
 }
