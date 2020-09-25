5c5004d990f3b5904079cfaf7785787a5812353a
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-25 11:37:05 +0100

    Passed
    testAWholePhrase

--- java/pig-latin/src/main/java/PigLatinTranslator.java
+++ java/pig-latin/src/main/java/PigLatinTranslator.java
@@ -1,43 +1,53 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class PigLatinTranslator {
 
-    String translate(String word) {
-        String pigWord = word;
+    String translate(String english) {
+        String pigWord;
 
-        String[] vowels = new String[] { "a", "e", "i", "o", "u", "yt", "xr" };
-        String[] doubleConsonants = new String[] { "ch", "qu", "th", "rh" };
-        String[] tripleConsonants = new String[] { "thr", "sch" };
+        final String[] vowels = new String[]{"a", "e", "i", "o", "u", "yt", "xr"};
+        final String[] doubleConsonants = new String[]{"ch", "qu", "th", "rh"};
+        final String[] tripleConsonants = new String[]{"thr", "sch"};
 
-        List<String> vowelsList = Arrays.asList(vowels);
-        List<String> doubleConsonantsList = Arrays.asList(doubleConsonants);
-        List<String> tripleConsonantsList = Arrays.asList(tripleConsonants);
+        final List<String> vowelsList = Arrays.asList(vowels);
+        final List<String> doubleConsonantsList = Arrays.asList(doubleConsonants);
+        final List<String> tripleConsonantsList = Arrays.asList(tripleConsonants);
 
-        String firstLetter = String.valueOf(word.charAt(0));
-        String secondLetter = "";
-        String thirdLetter = "";
+        ArrayList<String> pigWords = new ArrayList<String>();
+        String[] words = english.split("\\s+");
 
-        if (word.length() > 1) {
-            secondLetter = String.valueOf(word.charAt(1));
+        for (String word : words) {
+
+            String firstLetter = String.valueOf(word.charAt(0));
+            String secondLetter = "";
+            String thirdLetter = "";
+
+            if (word.length() > 1) {
+                secondLetter = String.valueOf(word.charAt(1));
+            }
+            if (word.length() > 2) {
+                thirdLetter = String.valueOf(word.charAt(2));
+            }
+
+            boolean isConstQu = secondLetter.equals("q") && thirdLetter.equals("u");
+
+            if (!vowelsList.contains(firstLetter) && isConstQu || tripleConsonantsList.contains(firstLetter + secondLetter + thirdLetter)) {
+                pigWord = word.substring(3) + firstLetter + secondLetter + thirdLetter;
+            } else if (doubleConsonantsList.contains(firstLetter + secondLetter)) {
+                pigWord = word.substring(2) + firstLetter + secondLetter;
+            } else if (!vowelsList.contains(firstLetter) && !vowelsList.contains(firstLetter + secondLetter)) {
+                pigWord = word.substring(1) + firstLetter;
+            } else {
+                pigWord = word;
+            }
+
+            pigWords.add(pigWord + "ay");
         }
-        if (word.length() > 2) {
-            thirdLetter = String.valueOf(word.charAt(2));
-        }
 
-        boolean isConstQu = secondLetter.equals("q") && thirdLetter.equals("u");
+        return String.join(" ", pigWords);
 
-        if ( !vowelsList.contains(firstLetter) && isConstQu || tripleConsonantsList.contains(firstLetter + secondLetter + thirdLetter)) {
-            pigWord = word.substring(3) + firstLetter + secondLetter + thirdLetter;
-        }
-        else if ( doubleConsonantsList.contains(firstLetter + secondLetter)) {
-            pigWord = word.substring(2) + firstLetter + secondLetter;
-        }
-        else if( !vowelsList.contains(firstLetter) && !vowelsList.contains(firstLetter + secondLetter) ) {
-            pigWord = word.substring(1) + firstLetter;
-        }
-
-        return pigWord + "ay";
     }
 
 }
