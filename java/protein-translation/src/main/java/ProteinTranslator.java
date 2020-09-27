8d91de673b716af7a06d5257d70d7e42a756027f
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-27 12:22:02 +0100

    Passed testMethionineRnaSequence

---
+++ java/protein-translation/src/main/java/ProteinTranslator.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+import java.util.List;
+
+class ProteinTranslator {
+
+    List<String> translate(String rnaSequence) {
+        return Arrays.asList("Methionine");
+    }
+}
