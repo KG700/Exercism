182eaed861e9f177ee490aa97c7ab1765fb4e32c
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-28 10:33:31 +0100

    Passed twoPiecesOfProverb

--- java/proverb/src/main/java/Proverb.java
+++ java/proverb/src/main/java/Proverb.java
@@ -8,7 +8,13 @@ class Proverb {
 
     String recite() {
         if (words.length == 0) return "";
-        return "And all for the want of a nail.";
+        String proverbStr = "";
+        if (words.length > 1) {
+            for (int i = 1; i < words.length; i++) {
+                proverbStr += "For want of a " + words[i-1] + " the " + words[i] + " was lost.\n";
+            }
+        }
+        return proverbStr + "And all for the want of a " + words[0] + ".";
     }
 
 }
