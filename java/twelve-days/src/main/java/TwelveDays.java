0ea51322019a519036f4aae9d4f170a9de41f107
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-29 09:54:46 +0100

    Refactored verse method to remove repetition

--- java/twelve-days/src/main/java/TwelveDays.java
+++ java/twelve-days/src/main/java/TwelveDays.java
@@ -1,26 +1,21 @@
+import java.util.Hashtable;
+
 class TwelveDays {
     String verse(int verseNumber) {
-        String verse = "";
+        Hashtable<Integer, String[]> gifts = new Hashtable<>();
+        gifts.put(1, new String[]{"first", "a Partridge in a Pear Tree"});
+        gifts.put(2, new String[]{"second", "two Turtle Doves"});
+        gifts.put(3, new String[]{"third", "three French Hens"});
+        gifts.put(4, new String[]{"fourth", "four Calling Birds"});
 
-        switch(verseNumber) {
-            case 1:
-                verse = "On the first day of Christmas my true love gave to me: " +
-                        "a Partridge in a Pear Tree.\n";
-                break;
-            case 2:
-                verse = "On the second day of Christmas my true love gave to me: two Turtle Doves, " +
-                        "and a Partridge in a Pear Tree.\n";
-                break;
-            case 3:
-                verse = "On the third day of Christmas my true love gave to me: three French Hens, " +
-                        "two Turtle Doves, and a Partridge in a Pear Tree.\n";
-                break;
-            case 4:
-                verse = "On the fourth day of Christmas my true love gave to me: four Calling Birds, " +
-                        "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n";
-                break;
+        String verse = "On the " + gifts.get(verseNumber)[0] + " day of Christmas my true love gave to me: ";
+        for (int i = verseNumber; i > 0; i--) {
+            verse += gifts.get(i)[1];
+            if (i > 1) verse += ", ";
+            if (i == 2) verse += "and ";
         }
-        return verse;
+
+        return verse + ".\n";
     }
 
     String verses(int startVerse, int endVerse) {
