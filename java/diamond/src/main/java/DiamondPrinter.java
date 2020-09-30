a078de0c0691ef805c2e535776e962d47ef5f0ec
Karla Gardiner <karla.gardiner@gmail.com>
2020-09-30 12:16:12 +0100

    Passed testTwoByTwoDiamond

--- java/diamond/src/main/java/DiamondPrinter.java
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -4,11 +4,24 @@ import java.util.List;
 class DiamondPrinter {
 
     List<String> printToList(char a) {
+        final String letters = "ABCD";
+        int dimension = letters.indexOf(a) * 2 + 1;
+        int middle = dimension / 2;
+
         ArrayList<String> diamond = new ArrayList<>();
 
-        diamond.add("A");
+        String row = "";
+        while (row.length() < dimension) row += " ";
+        while (diamond.size() < dimension) diamond.add(row);
 
+        for (int i = 0; i < middle + 1;  i++) {
+            StringBuilder newRow = new StringBuilder(row);
+            newRow.setCharAt(middle + i, letters.charAt(i));
+            newRow.setCharAt(middle - i, letters.charAt(i));
+            diamond.set(i, newRow.toString());
+            diamond.set(dimension - i - 1, newRow.toString());
+        }
         return diamond;
     }
-
 }
+
